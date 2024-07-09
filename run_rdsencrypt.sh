223aae51df4747c0aed29476b5e2e2f07901b300
mriccobene-git <mriccobene@aligntech.com>
2024-07-09 10:25:22 -0400

    Update run_rdsencrypt.sh
    added function to check if python is installed and if command is "python" or "python3"

--- run_rdsencrypt.sh
+++ run_rdsencrypt.sh
@@ -2,9 +2,24 @@
 
 # Function to check if a Python package is installed
 function is_installed {
-    python -c "import $1" &> /dev/null
+    $PYTHON_CMD -c "import $1" &> /dev/null
     return $?
 }
+
+# Function to find the python command
+function find_python {
+    if command -v python &> /dev/null; then
+        PYTHON_CMD=python
+    elif command -v python3 &> /dev/null; then
+        PYTHON_CMD=python3
+    else
+        echo "Python is not installed. Please install Python and try again."
+        exit 1
+    fi
+}
+
+# Find the python command
+find_python
 
 # Clone the repository
 git clone https://github.com/mriccobene-git/RDS-Encryption-Hardening.git
@@ -17,8 +32,8 @@ if is_installed boto3; then
     echo "boto3 is already installed."
 else
     echo "boto3 is not installed. Installing boto3..."
-    pip install boto3
+    $PYTHON_CMD -m pip install boto3
 fi
 
 # Run the Python script
-python rdsencrypt.py
+$PYTHON_CMD encrypt_rds_instances.py
